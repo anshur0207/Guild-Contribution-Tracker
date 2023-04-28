a902c9f2bd2ea0f5c0de14de59bee0bf5117d05b
Harshal Dubey <68217194+harshaldubey@users.noreply.github.com>
2023-04-28 15:45:02 +0530

    Updates in UI

--- Guild/src/Components/Work.js
+++ Guild/src/Components/Work.js
@@ -5,15 +5,14 @@ import anshu from "../Assets/boy2.png";
 import aditya from "../Assets/boy1.png";
 import jit from "../Assets/boy3.png";
 
-
 const Work = () => {
   const workInfoData = [
-   {
+    {
       image: harshal,
       title: "Harshal",
       text: "",
     },
-    
+
     {
       image: anshu,
       title: "Anshu",
@@ -39,16 +38,16 @@ const Work = () => {
     <div className="work-section-wrapper">
       <div className="work-section-top">
         <p className="primary-subheading">Developers</p>
-        <h3 className="primary-heading">How It Works- Because we worked 24/7</h3>
-        <p className="primary-text">
-          We worked very Hard for this.
-        </p>
+        <h3 className="primary-heading">
+          How It Works- Because we worked 24/7
+        </h3>
+        <p className="primary-text">We worked very Hard for this.</p>
       </div>
       <div className="work-section-bottom">
         {workInfoData.map((data) => (
           <div className="work-section-info" key={data.title}>
             <div className="info-boxes-img-container">
-              <img src={data.image} style={{height:'30vh'}}alt="" />
+              <img src={data.image} style={{ height: "30vh" }} alt="" />
             </div>
             <h2>{data.title}</h2>
             <p>{data.text}</p>
